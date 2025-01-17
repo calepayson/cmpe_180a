228de05e72925737026557a8a2d8968f7c504ad8
Cale Smith <calepayson@mac.com>
2025-01-17 12:05:58 -0800

    Modify cs_is_cool
    - Add complete ASCII art to the output.

--- savitch/ch_01/cs_is_cool/main.cpp
+++ savitch/ch_01/cs_is_cool/main.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 int main() {
+    cout << "**************************************************\n";
+    cout << '\n';
+    cout << "          C C C             S S S S        !!\n";
+    cout << "        C       C         S         S      !!\n";
+    cout << "       C                 S                 !!\n";
+    cout << "      C                   S                !!\n";
+    cout << "      C                    S S S S         !!\n";
+    cout << "      C                             S      !!\n";
+    cout << "       C                             S     !!\n";
+    cout << "        C       C         S         S\n";
+    cout << "          C C C             S S S S        00 \n";
+    cout << '\n';
+    cout << "**************************************************\n";
     cout << "    Computer Science is Cool Stuff!!!\n";
 
     return 0;
