667db57cd4b9a7bc65a2ef4e1d139d8e47850659
Cale Smith <calepayson@mac.com>
2025-01-21 13:13:03 -0800

    Add practice_problem_5
    - Refactor the given program to adhere to style guidelines.

---
+++ savitch/ch_02/practice_problem_5/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double radius, vm;
+    cout << "Enter radius of a sphere: ";
+    cin >> radius;
+    vm = (4.0 / 3.0) * 3.1415 * radius * radius * radius;
+    cout << " The volume is " << vm << endl;
+
+    return 0;
+}
