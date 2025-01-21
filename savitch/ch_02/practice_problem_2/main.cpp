cc35e5a494c9c0c8c6901c4447293af707cb7c04
Cale Smith <calepayson@mac.com>
2025-01-21 12:33:06 -0800

    Modify practice_problem_2
    - Add a function to get a non-zero integer from the user.

--- savitch/ch_02/practice_problem_2/main.cpp
+++ savitch/ch_02/practice_problem_2/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
+int get_int() {
+    int x{0};
+
+    std::cout << "Enter a non-zero integer: ";
+    std::cin >> x;
+
+    return x;
+}
+
 int main() {
     std::cout << "Welcome to square root.\n"
               << "Available modes: \n"
@@ -11,8 +20,12 @@ int main() {
 
     if (mode == 1) {
         std::cout << "You chose (1) Quick!\n";
+        int num{get_int()};
+        std::cout << "Calculating square root of " << num << "...\n";
     } else if (mode == 2) {
         std::cout << "You chose (2) Precise!\n";
+        int num{get_int()};
+        std::cout << "Calculating square root of " << num << "...\n";
     } else {
         std::cout << "Error: mode (" << mode << ") not recognized!\n";
     }
