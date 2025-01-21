8f0a9327384b2c57d35ccbd9ced16fdd47e357a1
Cale Smith <calepayson@mac.com>
2025-01-21 10:39:53 -0800

    Add example_3
    - Demonstrates use of the string class.

---
+++ savitch/ch_02/example_3/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string middle_name, pet_name;
+    string alter_ego_name;
+
+    cout << "Enter your middle name and the name of your pet. \n";
+    cin >> middle_name;
+    cin >> pet_name;
+
+    alter_ego_name = pet_name + ' ' + middle_name;
+
+    cout << "The name of your alter ego is ";
+    cout << alter_ego_name << ".\n";
+
+    return 0;
+}
