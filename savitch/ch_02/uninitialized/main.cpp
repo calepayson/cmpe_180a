8c20e112e768ec81bd4a0b3f182a0a92b9c588b2
Cale Smith <calepayson@mac.com>
2025-01-21 08:58:04 -0800

    Add uninitialized/
    - Demonstrates the importance of initializing variables.

---
+++ savitch/ch_02/uninitialized/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a, b, c, d, e;
+
+    std::cout << "a: " << a << '\n';
+    std::cout << "b: " << b << '\n';
+    std::cout << "c: " << c << '\n';
+    std::cout << "d: " << d << '\n';
+    std::cout << "e: " << e << '\n';
+
+    return 0;
+}
