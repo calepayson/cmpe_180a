c7c2889300f40b4f72063fc50cad356d56300c73
Cale Smith <calepayson@mac.com>
2025-01-21 12:41:02 -0800

    Modify practice_problem_2
    - Add a function to quickly calculate the square root of a number.
    - Uses the babylonian square root algorithm and makes 100 guesses.

--- savitch/ch_02/practice_problem_2/main.cpp
+++ savitch/ch_02/practice_problem_2/main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 
-int get_int() {
-    int x{0};
+double get_double() {
+    double x{0};
 
-    std::cout << "Enter a non-zero integer: ";
+    std::cout << "Enter a number: ";
     std::cin >> x;
 
     return x;
+}
+
+double quick_sqrt(double num) {
+    const int LIMIT{100};
+    int counter{0};
+    double guess{num / 2};
+    double r{};
+
+    while (counter < LIMIT) {
+        r = num / guess;
+        guess = (guess + r) / 2;
+        counter++;
+    }
+
+    return guess;
 }
 
 int main() {
@@ -18,13 +33,20 @@ int main() {
     int mode{};
     std::cin >> mode;
 
+    // Set floating point precision to four
+    std::cout.setf(std::ios::fixed);
+    std::cout.setf(std::ios::showpoint);
+    std::cout.precision(4);
+
     if (mode == 1) {
         std::cout << "You chose (1) Quick!\n";
-        int num{get_int()};
+        double num{get_double()};
         std::cout << "Calculating square root of " << num << "...\n";
+        double result{quick_sqrt(num)};
+        std::cout << "The square root of " << num << " is " << result << '\n';
     } else if (mode == 2) {
         std::cout << "You chose (2) Precise!\n";
-        int num{get_int()};
+        double num{get_double()};
         std::cout << "Calculating square root of " << num << "...\n";
     } else {
         std::cout << "Error: mode (" << mode << ") not recognized!\n";
