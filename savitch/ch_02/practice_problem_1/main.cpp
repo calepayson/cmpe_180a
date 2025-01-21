5c590f7e1dc928f0635525104abb27422fbc9ede
Cale Smith <calepayson@mac.com>
2025-01-21 12:18:17 -0800

    Modify practice_problem_1
    - Nest logic in a while loop to allow the user to repeat the
      calculation.

--- savitch/ch_02/practice_problem_1/main.cpp
+++ savitch/ch_02/practice_problem_1/main.cpp
@@ -3,19 +3,32 @@
 int main() {
     const double OUNCES_PER_TON{35273.92};
 
-    std::cout << "Enter the weight of a package of cereal in ounces: ";
-    double ounces{};
-    std::cin >> ounces;
+    char status{'y'};
 
-    double tons{ounces / OUNCES_PER_TON};
-    std::cout << "One box of cereal weighs " << tons << " metric tons.\n";
+    while (status == 'y' || status == 'Y') {
+        std::cout << "Enter the weight of a package of cereal in ounces: ";
+        double ounces{};
+        std::cin >> ounces;
 
-    std::cout.setf(std::ios::fixed);
-    std::cout.setf(std::ios::showpoint);
-    std::cout.precision(2);
+        // Set precision to 8
+        std::cout.setf(std::ios::fixed);
+        std::cout.setf(std::ios::showpoint);
+        std::cout.precision(8);
 
-    double boxes_per_ton{1 / tons};
-    std::cout << boxes_per_ton << " boxes needed to yield 1 metric ton.\n";
+        double tons{ounces / OUNCES_PER_TON};
+        std::cout << "One box of cereal weighs " << tons << " metric tons.\n";
+
+        // Set precision to 2
+        std::cout.setf(std::ios::fixed);
+        std::cout.setf(std::ios::showpoint);
+        std::cout.precision(2);
+
+        double boxes_per_ton{1 / tons};
+        std::cout << boxes_per_ton << " boxes needed to yield 1 metric ton.\n";
+
+        std::cout << "Continue? (y/n): ";
+        std::cin >> status;
+    }
 
     return 0;
 }
