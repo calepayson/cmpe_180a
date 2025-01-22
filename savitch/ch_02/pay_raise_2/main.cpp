e3189bb9cf607465c9b62fb76289c4ab5ad49450
Cale Smith <calepayson@mac.com>
2025-01-22 08:52:40 -0800

    Add pay_raise_2
    - Satisfies programming project 3.
    - Same as pay_raise but also allows the user to set months of
      retroactive pay.

---
+++ savitch/ch_02/pay_raise_2/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+    const double PAY_RAISE = 1.076;
+
+    std::cout << "Previous annual salary: $";
+    double previous_annual_salary{};
+    std::cin >> previous_annual_salary;
+
+    std::cout << "Months of retroactive pay: ";
+    int retroactive_months{};
+    std::cin >> retroactive_months;
+
+    // Set output precision to two
+    std::cout.setf(std::ios::fixed);
+    std::cout.setf(std::ios::showpoint);
+    std::cout.precision(2);
+
+    double retroactive_pay =
+        (previous_annual_salary * PAY_RAISE - previous_annual_salary) *
+        retroactive_months / 12;
+    std::cout << "Retroactive pay: $" << retroactive_pay << '\n';
+
+    double new_annual_salary = previous_annual_salary * PAY_RAISE;
+    std::cout << "New annual salary: $" << new_annual_salary << '\n';
+
+    double new_monthly_salary = new_annual_salary / 12;
+    std::cout << "New monthly salary: $" << new_monthly_salary << '\n';
+
+    return 0;
+}
