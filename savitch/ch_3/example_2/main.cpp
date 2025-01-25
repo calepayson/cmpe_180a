7c1e77d2112ccfffdd3833f22bac3f328d1bec5f
Cale Smith <calepayson@mac.com>
2025-01-24 16:08:27 -0800

    Add example_2

--- savitch/ch_3/example_2/main.cpp
+++ savitch/ch_3/example_2/main.cpp
@@ -11,18 +11,23 @@ int main() {
 
     switch (grade) {
         case 'A':
+        case 'a':
             cout << "Excellent. You need not take the final.\n";
             break;
         case 'B':
+        case 'b':
             cout << "Very good. ";
             grade = 'A';
             cout << "Your midterm grade is now " << grade << endl;
             break;
         case 'C':
+        case 'c':
             cout << "Passing.\n";
             break;
         case 'D':
+        case 'd':
         case 'F':
+        case 'f':
             cout << "Not good. Go study.\n";
             break;
         default:
