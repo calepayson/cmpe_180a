c5e6eb430502671c9d40f97f9c3650589d95371f
Cale Smith <calepayson@mac.com>
2025-02-06 16:45:32 -0800

    Add get/
    - Demonstrates the use of the get command.

---
+++ savitch/ch_06/get/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout << "Enter a line of input and I will echo it!\n";
+    char symbol;
+    do {
+        cin.get(symbol);
+        cout << symbol;
+    } while (symbol != '\n');
+    cout << "That's all for this demonstration.\n";
+
+    return 0;
+}
