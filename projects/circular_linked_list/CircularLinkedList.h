6106491c779e268f98fdfa898664e2ef1d94a05d
Cale Smith <calepayson@mac.com>
2025-04-22 08:56:57 -0700

    Create circular_linked_list.
    - Add CircularLinkedList.h
    - Declaration of a CircularLinkedList class.

---
+++ projects/circular_linked_list/CircularLinkedList.h
@@ -0,0 +1,45 @@
+#ifndef CIRCULARLINKEDLIST_H
+#define CIRCULARLINKEDLIST_H
+
+#include <iostream>
+
+using std::ostream;
+
+struct llNode {
+    int data;
+    llNode *prev;
+    llNode *next;
+};
+
+enum ClockDirection { ClockWise, CounterClockWise };
+
+class CircularLinkedList {
+   public:
+    // Default Constructor
+    CircularLinkedList();
+
+    // Destructor
+    virtual ~CircularLinkedList();
+
+    // push: Insert a new node containing the integer provided in the d
+    // direction.
+    void push(int i, ClockDirection d);
+
+    // pop: Removes the head node and returns its value and sets the head in the
+    // direction specified in d.
+    int pop(ClockDirection d);
+
+    // peek: Return the integer in the head of the circular list.
+    int peek() const;
+
+    // rotate: Rotate the head pointer n spaces in the direction specified in d.
+    void rotate(unsigned int n, ClockDirection d);
+
+    // Overload << operator
+    friend ostream &operator<<(ostream &os, const CircularLinkedList &list);
+
+   private:
+    llNode *head;
+};
+
+#endif
